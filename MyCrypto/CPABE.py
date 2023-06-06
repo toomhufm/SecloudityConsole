f25f550876c315a7c8e88cfe5bf49009e322d62f
toomhufm <ntad.a2.vt@gmail.com>
2023-06-06 20:19:05 +0700

    Cleanup CPABE Code

--- MyCrypto/CPABE.py
+++ MyCrypto/CPABE.py
@@ -50,38 +50,25 @@ def ABEencryption(filename,pk,policy):
     session_key = groupObj.random(GT)
     session_key_ctxt = cpabe.encrypt(pk,session_key,policy)
 
-    # for i in session_key_ctxt.values(): print((i))
 
     session_key_ctxt_b = serialize_encoder.jsonify_ctxt(session_key_ctxt)
     session_key_ctxt_b = base64.b64encode(session_key_ctxt_b.encode())
     session_key_size = len(session_key_ctxt_b)
-    # print("Session Key " , (session_key_ctxt_b))
-    # print(f" Session key size : {session_key_size}")
-    # output = open(f"{filename}.scd","wb")
     stream = struct.pack('Q',session_key_size)
-    # output.write(stream)
     namesplit = filename.split('/')
     outname = f"{namesplit[len(namesplit)-1]}.scd"
 
     """
-    Use AES-GCM to encrypt the file then attach needed component
+    Use AES to encrypt the file then attach needed component
     """
 
     aes_key = hashlib.sha256(str(session_key).encode()).digest()
     iv = os.urandom(16)
 
-    # print(f"Key : {aes_key}")
-    # print(f"IV : {iv}")
     encryptor = AES.new(aes_key,AES.MODE_CFB,iv)
     encrypted_data = encryptor.encrypt(msg)
     output = stream + iv + session_key_ctxt_b + encrypted_data
-    # print(f"Encrypted : {encrypted_data} ")
-    # print("Session Key Length : ",len(session_key_ctxt_b))
-    # output = open("encrypted.scd","wb")
-    # output.write(iv)
-    # output.write(session_key_ctxt_b)
-    # output.write(encrypted_data)
-    # output.close()
+
     return output,outname.encode()
 
 
@@ -91,38 +78,24 @@ def ABEdecryption(filename,pk,sk):
     session_key_size = struct.unpack('Q',ciphertext_stream.read(struct.calcsize('Q')))[0]
     ciphertext_stream.close()
     ciphertext = open(filename,"rb").read()
-    # print(f" Session key size : {session_key_size}")
     iv = ciphertext[8:24]
-    # print(f"IV : {iv}")
     session_key_ctxt_b = ciphertext[24:session_key_size+24]
     session_key_ctxt_b = base64.b64decode(session_key_ctxt_b)
-    # print("Session Key B64 : ",session_key_ctxt_b)
     session_key_ctxt = serialize_encoder.unjsonify_ctxt(session_key_ctxt_b)
     session_key = cpabe.decrypt(pk,session_key_ctxt,sk)
-    # bytesToObject(session_key_ctxt_b,groupObj)
-    # print(session_key)  
     output_filename = filename.replace('./ServerStorage/','').replace('.scd','')
     if(session_key):
         aes_key = hashlib.sha256(str(session_key).encode()).digest()
         encryptor = AES.new(aes_key,AES.MODE_CFB,iv)
         decrypted_data = encryptor.decrypt(ciphertext[8+16+session_key_size:])
-        # with open(f"/mnt/f/Cryptography/Projects/SecloudityConsole/Downloads/{output_filename}","wb") as f :
-        #     f.write(decrypted_data)
+
         return decrypted_data
     else:
         print("Policy not satisfied!")
 
-    # session_key = cpabe.decrypt()
-
 def LoadKey(key):
     key = bytesToObject(key,groupObj)
     return key
-
-# def SaveKey(path_to_pk,pk,path_to_sk,sk,groupObj):
-#     with open(path_to_pk,"wb") as f:
-#         f.write(objectToBytes(pk,groupObj))
-#     with open(path_to_sk,"wb") as f:
-#         f.write(objectToBytes(sk,groupObj))
 
 def KeyToBytes(pk,mk):
     pkb = objectToBytes(pk,groupObj)
