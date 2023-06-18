b8882f6bcf9863a7f708f9733c0dcc5d2005f6d5
toomhufm <ntad.a2.vt@gmail.com>
2023-06-18 22:28:00 +0700

    Update Search feature

--- Client.py
+++ Client.py
@@ -146,6 +146,49 @@ def HandleSearch(message):
                 msg += f' -n {filename}'
     return msg
 
+def Search(message):
+    option = ['-d','-n','-o']
+    message = message.split(' ')
+    op = [0]*3
+    for msg in message:
+        if msg in option:
+            if(msg == '-d'):
+                op[0] = message[message.index(msg) + 1]
+            if(msg == '-n'):
+                op[1] = message[message.index(msg) + 1]
+            if(msg == '-o'):
+                op[2] = message[message.index(msg) + 1]
+    filter = {}
+    if(op[0]):
+        filter['upload_date'] = op[0]
+    if(op[1]):
+        filter['filename'] = op[1]
+    if(op[2]):
+        filter['owner'] = op[2]
+    action = url + "find"
+    payload = json.dumps({
+        "collection": "Documents",
+        "database": "CompanyStorage",
+        "dataSource": "Cluster0",
+        "filter" : filter,
+        "projection":{
+            "filename":1,
+            "owner":1,
+            "upload_date":1,
+            "sha256":1
+        }
+    })    
+    
+    response = requests.request("POST", action, headers=headers, data=payload)
+    result = json.loads(response.text)['documents']
+    if(result):
+        to_print = len(result)*[0]
+        for i in range(len(result)):
+            to_print[i] = result[i]['_id'], result[i]['filename'],result[i]['owner'],result[i]['upload_date'],result[i]['sha256']
+        print(tabulate(to_print,headers=['ID','File Name','Owner','Upload Date','SHA256'],tablefmt='grid'))
+    else:
+        print("[!] Found 0 file")
+
 def client_receive():
     global LogedIn
     global Verified
@@ -244,7 +287,8 @@ def handle_input(message : str):
                             print("[!] : Too many options!")
                         else:
                             msg = HandleSearch(message)
-                            return msg.encode()        
+                            Search(msg)
+                            return None       
                     return None 
             else:
                 print("[!] : You must verified first")
